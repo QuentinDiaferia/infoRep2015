92c802b997edd2461819ee70fa9fe64f048a0e8b
GuillaumeDSM <guillaume.minette_de_saintmartin@insa-rouen.fr>
2015-04-04 14:51:06 +0200

    ajout de la classe bueau

---
+++ code/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+javac -sourcepath ./src -d ./classes ./src/bureau/*.java
+echo "    1. Bureau compilé"
---
+++ code/src/bureau/Bureau.java
@@ -0,0 +1,36 @@
+package bureau;
+
+
+
+public class Bureau {
+
+	private int nbUtilisateurs;
+	private int nbWidgets;
+	private static final int nbMaxUtilisateurs = 4;
+	private static final int nbMaxWidgets = 6;
+
+
+
+	public int getnbUtilisateurs(){
+		return this.nbUtilisateurs ;
+	}
+
+	public int getnbWidgets(){
+		return this.nbWidgets ;
+	}
+
+	public int setnbUtilisateurs(int nb){
+		this.nbUtilisateurs = nb ;
+		return this.nbUtilisateurs ;
+	}
+
+	public int setnbWidgets(int nb){
+		this.nbWidgets = nb ;
+		return this.nbWidgets ;
+	}
+
+	public String toString() {
+		String result = new String("Bureau possédant:\n"+ getnbWidgets() +" widgets et dont le nombre maximum est: "+nbMaxWidgets+"\n"+getnbUtilisateurs()+ "nbUtilisateurs et dont le nombre maximum est: "+nbMaxUtilisateurs);
+		return result;
+	}
+}
