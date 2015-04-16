0ba27ba5d03f5939c77895ac2ba244cc0ae234ed
FLepetit <contact@florianlepetit.fr>
2015-04-16 15:51:24 +0200

    fix compilation
    reste à savoir si le délire des imports vient de moi

--- developpement/Semaine1/BureauPartage/compile.sh
+++ developpement/Semaine1/BureauPartage/compile.sh
@@ -1,3 +1,3 @@
-javac -sourcepath src/ -cp classes/ -d classes/ Util/src/Bureau.java
-javac -sourcepath src/ -cp classes/ -d classes/ Serveur/src/ServeurConnexionsMultiples.java
-javac -sourcepath src/ -cp classes/ -d classes/ Client/src/Client.java
+javac -sourcepath Util/src/ -cp Util/src/ -d Util/classes/ Util/src/*.java
+javac -sourcepath Serveur/src/ -cp Serveur/src/ -d Serveur/classes/ Serveur/src/*.java
+javac -sourcepath Client/src/ -cp Client/src/ -d Client/classes/ Client/src/*.java
