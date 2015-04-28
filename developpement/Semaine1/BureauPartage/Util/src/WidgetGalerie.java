1452d68e7a8f28aa00cd647b795c5817662b3a4d
ggutzwiller <gregoire.gutzwiller@insa-rouen.fr>
2015-04-28 16:34:30 +0200

    Ajout de la classe WidgetGalerie

---
+++ developpement/Semaine1/BureauPartage/Util/src/WidgetGalerie.java
@@ -0,0 +1,53 @@
+package bureau;
+
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class WidgetGalerie extends Widget implements Serializable{
+    private Integer image;
+    private HashMap<Integer, String> liensImages = new HashMap<>();   
+    
+    public WidgetGalerie(boolean statut, String nom, int x, int y, int z){
+        super(statut, nom, x, y, z);
+        this.image = 1 ;
+        this.liensImages.put(1, "lien");
+    }
+    
+    public Integer getImage(){
+        return this.image;
+    }
+
+    public String getImageActuelle(){
+        return this.liensImages.get(this.image);
+    }
+
+    public HashMap<Integer, String> getImages(){
+        return this.liensImages;
+    }
+
+    public void setImage(Integer _image){
+        this.image = _image;
+    }
+
+    public void suivant(){
+        if (this.liensImages.size() > this.image) {
+            this.image = this.image + 1;
+        } else {
+            this.image = 1;
+        }
+    }
+        
+    public void precedent(){
+        if (this.image == 1) {
+            this.image = this.liensImages.size();
+        } else {
+            this.image = this.image - 1;
+        }
+    }
+
+    public String toString() {
+        String result = new String(super.toString()+"\n Galerie affichant l'image à l'adresse : "+this.liensImages.get(this.image));
+		return result;
+    }
+}
