465cc00e64b6d2409b0aaf96910f662620e6d456
QuentinDiaferia <q.diaferia@gmail.com>
2015-05-09 17:22:19 +0200

    Logique métier bloc note

--- developpement/Util/src/bureau/WidgetBlocNote.java
+++ developpement/Util/src/bureau/WidgetBlocNote.java
@@ -2,28 +2,38 @@ package bureau;
 
 import java.lang.*;
 import java.io.*;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
-import javax.imageio.ImageIO;
 import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class WidgetBlocNote extends Widget implements Serializable{
     private String contenu;
     private final JPanel panneau;
-    private JTextArea jta;;
+    private JTextArea jta;
+    private JScrollPane jsp;
 
     public WidgetBlocNote(boolean statut, String nom){
         super(statut, nom);
         this.contenu =  "Vous pouvez écrire ici.";
         panneau = new JPanel();
         
-        jta = new JTextArea(15, 20);
+        jta = new JTextArea(contenu, 15, 20);
         jta.setLineWrap(true);
-        JScrollPane scrollbar = new JScrollPane(jta);
-		scrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        
-        panneau.add(scrollbar);
+        jsp = new JScrollPane(jta);
+		jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        jta.addFocusListener(new FocusListener() {    
+			public void focusGained(FocusEvent e) {
+				System.out.println("Focus");
+				setStatut(true);
+				jta.addKeyListener(new ToucheListener());
+			}
+
+			public void focusLost(FocusEvent e) {
+				System.out.println("Focus out");
+				setStatut(false);
+			}
+        });
+        panneau.add(jsp);
         this.setContentPane(panneau);
         this.setSize(265,275);
     }
@@ -40,4 +50,17 @@ public class WidgetBlocNote extends Widget implements Serializable{
         String result = new String(super.toString()+"\n Bloc note contenant le texte suivant : "+this.contenu);
         return result;
     }
+    
+    class ToucheListener implements KeyListener {
+		public void keyReleased(KeyEvent e) {
+			setContenu(jta.getText());
+			System.out.println(contenu);
+		}
+		public void keyPressed(KeyEvent e) {
+			
+		}
+		public void keyTyped(KeyEvent e) {
+			
+		}	
+	}
 }
