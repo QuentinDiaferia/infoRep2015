be68a1d3c5306cacf3167cbb50c5e2f35985bb4c
ggutzwiller <gregoire.gutzwiller@insa-rouen.fr>
2015-05-09 18:57:49 +0200

    IHM du Widget Galerie

--- developpement/Util/src/bureau/WidgetGalerie.java
+++ developpement/Util/src/bureau/WidgetGalerie.java
@@ -2,16 +2,70 @@ package bureau;
 
 import java.lang.*;
 import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
 import java.util.*;
 
 public class WidgetGalerie extends Widget implements Serializable{
     private Integer image;
     private HashMap<Integer, String> liensImages = new HashMap<>();
+    private final JPanel panneau;
 
     public WidgetGalerie(boolean statut, String nom){
         super(statut, nom);
         this.image = 1 ;
-        this.liensImages.put(1, "lien");
+        this.liensImages.put(1, "../Images/galerie1.jpg");
+        this.liensImages.put(2, "../Images/meteo.png");
+        panneau = new JPanel(){
+            public void paintComponent(Graphics g){
+                try{
+                    super.paintComponent(g);
+                    g.drawImage(ImageIO.read(new File(getImageActuelle())), 0, 0, this);
+                }
+                catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        };
+
+        add(panneau, BorderLayout.SOUTH);
+        this.setContentPane(panneau);
+        this.setSize(250,330);
+
+        JButton precedent = new JButton("<");
+        JButton suivant = new JButton(">");
+        JPanel menu = new JPanel();
+        menu.setLayout(new GridLayout(1,2));
+        menu.add(precedent);
+        menu.add(suivant);
+        add(menu, BorderLayout.NORTH);
+
+        //register listener
+        PrecedentButtonListener precedentButton = new PrecedentButtonListener ();
+        SuivantButtonListener suivantButton = new SuivantButtonListener ();
+
+        //add listeners to corresponding componenets 
+        precedent.addActionListener(precedentButton);
+        suivant.addActionListener(suivantButton);
+
+    }
+
+    class PrecedentButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e)
+            {
+                precedent();
+                repaint();
+            }
+    }
+
+    class SuivantButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e)
+            {
+                suivant();
+                repaint();
+            }
     }
 
     public Integer getImage(){
